a7be0ddf03174460032a2661a04bba5d777a0aaa
Raja Chakraborty <rjchakraborty21@gmail.com>
2026-01-10 15:38:49 +0530

    Modify startxfce4_debian.sh for Termux compatibility
    Updated script for Termux X11 environment setup, including changes to paths, process management, and PulseAudio initialization.

--- startxfce4_debian.sh
+++ startxfce4_debian.sh
@@ -1,34 +1,27 @@
-#!/bin/bash
+#!/data/data/com.termux/files/usr/bin/bash
 
-# Display (do NOT change unless you know why)
-export DISPLAY=:0
+# Kill open X11 processes
+kill -9 $(pgrep -f "termux.x11") 2>/dev/null
 
-# Required runtime dir (GNOME/XFCE both need this)
-export XDG_RUNTIME_DIR=/tmp/runtime-$USER
-mkdir -p $XDG_RUNTIME_DIR
-chmod 700 $XDG_RUNTIME_DIR
+# Enable PulseAudio over Network
+pulseaudio --start --load="module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1" --exit-idle-time=-1
 
-# Prevent accessibility + system bus noise
-export NO_AT_BRIDGE=1
-export DBUS_SESSION_BUS_ADDRESS=
+# Prepare termux-x11 session
+export XDG_RUNTIME_DIR=${TMPDIR}
+termux-x11 :0 >/dev/null &
 
-# XFCE must not try Wayland or fancy backends
-export XDG_SESSION_TYPE=x11
-export GDK_BACKEND=x11
-export QT_QPA_PLATFORM=xcb
+# Wait a bit until termux-x11 gets started.
+sleep 3
 
-# Hard cleanup (important)
-pkill -9 xfce4-session 2>/dev/null
-pkill -9 xfwm4 2>/dev/null
-pkill -9 xfce4-panel 2>/dev/null
-pkill -9 dbus-daemon 2>/dev/null
+# Launch Termux X11 main activity
+am start --user 0 -n com.termux.x11/com.termux.x11.MainActivity > /dev/null 2>&1
+sleep 1
 
-# Disable components that REQUIRE systemd
-rm -f ~/.config/autostart/light-locker.desktop
-rm -f ~/.config/autostart/xfce4-power-manager.desktop
+# Login in PRoot Environment. Do some initialization for PulseAudio, /tmp directory
+# and run XFCE4 as user droidmaster.
+# See also: https://github.com/termux/proot-distro
+# Argument -- acts as terminator of proot-distro login options processing.
+# All arguments behind it would not be treated as options of PRoot Distro.
+proot-distro login debian --shared-tmp -- /bin/bash -c  'export PULSE_SERVER=127.0.0.1 && export XDG_RUNTIME_DIR=${TMPDIR} && su - droidmaster -c "env DISPLAY=:0 startxfce4"'
 
-# Clear broken sessions
-rm -rf ~/.cache/sessions/*
-
-# Start XFCE with a session-only DBus
-exec dbus-launch --exit-with-session startxfce4
+exit 0
